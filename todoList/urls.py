170eb55f08593a6b402ad02e25f9d4b2676b0bd9
hisabuz <hisabuz@gmail.com>
2023-06-11 18:23:53 -0400

    Models created 2

--- todoList/urls.py
+++ todoList/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask, name='addTask')
+    path('addTask/', views.addTask, name='addTask'),
+    path('mark_as_done/<int:id>',views.mark_as_done, name='mark_as_done' ),
+    path('undo_mark_as_done/<int:id>', views.undo_mark_as_done, name='undo_mark_as_done'),
+
+    # Edit feature
+    path('edit_task/<int:id>/', views.edit_task, name="edit_task"),
+
+    # Delete feature
+    path('delete_task/<int:id>/', views.delete_task, name="delete_task")
 ]
--- todoList/views.py
+++ todoList/views.py
@@ -8,3 +8,35 @@ def addTask(request):
     task = request.POST['task']   
     Task.objects.create(task=task)
     return redirect('home')
+
+
+def mark_as_done(request, id):
+    task = Task.objects.get(pk=id)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def undo_mark_as_done(request, id):
+    task = Task.objects.get(pk=id)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+
+def edit_task(request, id):
+    get_task = Task.objects.get(pk=id)
+    if request.method == "POST":
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task
+        }
+    return render(request, 'edit_task.html', context)
+
+def delete_task(request, id):
+    task = Task.objects.get(pk=id)
+    task.delete()
+    return redirect('home')
